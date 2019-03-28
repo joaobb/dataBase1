483df816ad95e826015a8ab52b418b9c0f8fbce3
joaobb <joaopedro.bb98@gmail.com>
2019-03-28 11:47:06 -0300

    Andamento roteiro2

--- roteiro2-117210327.sql
+++ roteiro2-117210327.sql
@@ -31,4 +31,46 @@ ALTER TABLE tarefas ALTER COLUMN prioridade TYPE SMALLINT;
 
 --questao_4
 DELETE FROM tarefas WHERE descricao IS null;
-ALTER TABLE tarefas ALTER COLUMN * SET NOT NULL;
+ALTER TABLE tarefas ALTER COLUMN id SET NOT NULL;
+ALTER TABLE tarefas ALTER COLUMN descricao SET NOT NULL;
+ALTER TABLE tarefas ALTER COLUMN func_resp_cpf SET NOT NULL;
+ALTER TABLE tarefas ALTER COLUMN prioridade SET NOT NULL;
+ALTER TABLE tarefas ALTER COLUMN status SET NOT NULL;
+
+--questao_5
+INSERT INTO tarefas VALUES (2147483653, 'limpar portas do 1o andar','32323232911',2,'A');
+INSERT INTO tarefas VALUES (2147483653, 'aparar a grama da área frontal','32323232911', 3, 'A');
+--INSERT 0 1
+DELETE FROM tarefas WHERE (id = 2147483653 AND descricao = 'limpar portas do 1o andar');
+--DELETE 1
+ALTER TABLE tarefas ADD CONSTRAINT tarefas_pk PRIMARY KEY (id, func_resp_cpf);
+
+--questao_6
+---a)
+INSERT INTO tarefas VALUES (2147483658, 'limpar portas do 1o andar','323232324832',1,'A');
+--ERROR:  value too long for type character(11)
+---Ja ha a impossibilidade de insercao de CPFs de tamanhos destintos de 11, pelo fato,
+---do atributo ter sido definindo com CHAR(11), logo, este apenas aceita valores com 11 chars.
+ALTER TABLE tarefas ADD CONSTRAINT cpf_validity CHECK (LENGTH(func_resp_cpf) = 11);
+
+---b)
+ALTER TABLE tarefas ADD CONSTRAINT possibleStatus CHECK (status = 'P' OR status = 'E' OR status = 'C');
+--ERROR:  check constraint "possiblestatus" is violated by some row
+
+UPDATE tarefas SET status = 'P' WHERE status = 'A';
+UPDATE tarefas SET status = 'E' WHERE status = 'R';
+UPDATE tarefas SET status = 'C' WHERE status = 'F';
+
+--questao_7
+DELETE FROM tarefas WHERE (prioridade > 5 OR prioridade < 0);
+ALTER TABLE tarefas ADD CONSTRAINT priority_limit CHECK(prioridade >= 0 OR prioridade <= 5);
+
+--questao_8
+CREATE TABLE funcionario(
+    cpf CHAR(11) PRIMARY KEY,
+    data_nasc DATE,
+    nome TEXT,
+    funcao TEXT,
+    nivel CHAR(1),
+    superior_cpf CHAR(11) REFERENCES funcionario(cpf)
+);
---
+++ ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh  -o ServerAliveInterval=30 joaopdbb@150.165.85.37 -p 45600
