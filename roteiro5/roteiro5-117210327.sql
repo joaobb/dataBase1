5343d634af27455e4b36e70864a8ba10439dbe26
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2019-10-01 21:34:53 -0300

    futhering

--- roteiro5/roteiro5-117210327.sql
+++ roteiro5/roteiro5-117210327.sql
@@ -1,42 +1,46 @@
 --Q1
-SELECT COUNT(sex) FROM employee WHERE sex = 'F';
+SELECT COUNT(*) 
+FROM employee 
+WHERE sex = 'F';
 
 --Q2
-SELECT AVG(salary) FROM employee WHERE address LIKE '%, TX' AND sex = 'M';
+SELECT AVG(salary) 
+FROM employee 
+WHERE address LIKE '%TX'AND sex = 'M';
 
 --Q3
-SELECT e.superssn as ssn_supervisor, COUNT(e.ssn) AS qtd_supervisionados
-FROM employee AS e
-GROUP BY e.superssn
-ORDER BY COUNT(*) ASC;
-
+SELECT S.ssn AS ssn_supervisor, COUNT(*) AS qtd_supervisionados 
+FROM employee AS S RIGHT OUTER JOIN employee AS E ON S.ssn = E.superssn 
+GROUP BY S.ssn 
+ORDER BY qtd_supervisionados ASC;
 
 --Q4
-SELECT m.fname as nome_supervisor, COUNT(*) AS qtd_supervisionados 
-FROM (employee AS m JOIN employee AS e ON m.ssn = e.superssn)
-GROUP BY m.ssn
-ORDER BY COUNT(*) ASC;
+SELECT S.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados 
+FROM employee AS S JOIN employee AS E ON E.superssn = S.ssn 
+GROUP BY S.fname 
+ORDER BY qtd_supervisionados ASC;
 
 --Q5
-SELECT m.fname as nome_supervisor, COUNT(*) AS qtd_supervisionados 
-FROM (employee AS m RIGHT OUTER JOIN employee AS e ON m.ssn = e.superssn)
-GROUP BY m.ssn
-ORDER BY COUNT(*) ASC;
+SELECT S.fname AS nome_supervisor, COUNT(*) AS qtd_supervisionados 
+FROM employee AS S RIGHT OUTER JOIN employee AS E ON S.ssn = E.superssn 
+GROUP BY S.fname 
+ORDER BY qtd_supervisionados ASC;
 
 --Q6
-SELECT MIN(count) AS qtd
+SELECT MIN(qtd) AS qtd 
 FROM (
-    SELECT COUNT(*)
-    FROM works_on
-    GROUP BY pno
-    ) AS res;
+    SELECT p.pnumber, COUNT(*) AS qtd 
+    FROM project AS p, works_on AS W 
+    WHERE p.pnumber = w.pno 
+    GROUP BY p.pnumber) 
+AS fun;
 
 --Q7
-SELECT pno as num_projeto, qtd as qtd_func
+SELECT pno AS num_projeto, qtd AS qtd_func
 FROM (
     (SELECT pno, COUNT(*)
     FROM works_on
-    GROUP BY pno) as pnc
+    GROUP BY pno) AS pnc
     
     JOIN
     
@@ -51,80 +55,67 @@ FROM (
 );
 
 --Q8
-SELECT w.pno AS num_projeto, AVG(e.salary) AS media_sal
-FROM works_on as w JOIN employee as e ON (w.essn = e.ssn)
-GROUP BY w.pno;
+SELECT w.pno AS num_projeto, AVG(e.salary) AS media_sal 
+FROM works_on AS w, employee AS e 
+WHERE e.ssn = w.essn GROUP BY w.pno;
 
 --Q9
-SELECT w.pno AS proj_num, p.pname as proj_nome, AVG(e.salary) AS media_sal
-FROM project as p JOIN (works_on as w JOIN employee as e ON (w.essn = e.ssn)) ON (p.pnumber = w.pno)
-GROUP BY w.pno, p.pname
-ORDER BY AVG(e.salary) ASC;
+SELECT p.pnumber AS num_projeto, p.pname AS proj_nome, AVG(e.salary) AS media_sal 
+FROM works_on AS w, employee AS e, project AS p 
+WHERE e.ssn = w.essn AND w.pno = p.pnumber 
+GROUP BY p.pnumber;
 
 --Q10
-SELECT e.fname, e.salary
-FROM employee AS e
+SELECT e.fname, e.salary 
+FROM employee as e 
 WHERE e.salary > ALL (
-SELECT e.salary
-FROM works_on as w JOIN employee AS e ON (w.essn = e.ssn AND w.pno = 92));
+    SELECT a.salary 
+    FROM works_on AS w, employee AS a 
+    WHERE a.ssn = w.essn AND w.pno = 92);
 
 --Q11
-SELECT e.ssn, COUNT(w.pno)
-FROM employee as e LEFT OUTER JOIN works_on as w ON (e.ssn = w.essn)
-GROUP BY e.ssn
-ORDER BY COUNT(w.pno) ASC;
+SELECT e.ssn, COUNT(w.essn) AS qtd_proj 
+FROM employee AS e LEFT OUTER JOIN works_on AS w ON e.ssn = w.essn 
+GROUP BY e.ssn 
+ORDER BY qtd_proj ASC;
 
 --Q12
-SELECT pno as num_proj, count as qtd_func
-FROM (
-    SELECT pno, COUNT(*)
-    FROM employee AS e LEFT OUTER JOIN works_on AS w ON (w.essn = e.ssn) 
-    GROUP BY pno
-) as sel
-WHERE sel.count < 5;
+SELECT w.pno AS num_projeto, COUNT(*) AS qtd_fun 
+FROM employee AS e LEFT OUTER JOIN works_on AS w ON e.ssn = w.essn 
+GROUP BY w.pno HAVING COUNT(*) < 5;
 
 --Q13
-SELECT fname
-FROM (
-    SELECT eS.ssn
-    FROM (
-        SELECT e.ssn
-        FROM (
-            SELECT w.essn
-            FROM (
-                SELECT p.pnumber as pnumb
-                FROM project as p
-                WHERE p.plocation = 'Sugarland'
-            ) AS proj, works_on as w
-            WHERE w.pno = proj.pnumb
-        ) AS sW, employee as e
-        WHERE sW.essn = e.ssn
-    ) AS eS, dependent AS d
-    WHERE eS.ssn = d.essn
-    GROUP BY eS.ssn
-) AS fin, employee as e
-WHERE fin.ssn = e.ssn; 
+SELECT e.fname 
+FROM employee AS e 
+WHERE EXISTS(
+    SELECT * 
+    FROM dependent 
+    WHERE e.ssn = essn) 
+    AND 
+    EXISTS (
+        SELECT * 
+        FROM dept_locations AS l, department AS d 
+        WHERE e.dno = d.dnumber AND d.dnumber = l.dnumber AND l.dlocation = 'Sugarland');
 
 --Q14
-SELECT d.dname
-FROM department as d 
-WHERE NOT EXISTS(
-    SELECT *
-    FROM project as p
-    WHERE p.dnum = d.dnumber
-);
+SELECT d.dname 
+FROM department AS d 
+WHERE NOT EXISTS (
+    SELECT * 
+    FROM project AS p 
+    WHERE d.dnumber = p.dnum);
 
 --Q15
-SELECT e.fname, e.lname
-FROM employee AS e
+SELECT e.fname, e.lname 
+FROM employee AS e 
 WHERE NOT EXISTS(
-    (SELECT w.pno
-    FROM works_on as w
-    WHERE w.essn = '123456789')
+    (SELECT w.pno 
+    FROM works_on AS w 
+    WHERE w.essn = '123456789') 
+    EXCEPT(
+        SELECT pno 
+        FROM works_on 
+        WHERE e.ssn = essn AND NOT(e.ssn = '123456789')
+        )
+    );
 
-    EXCEPT
-
-    (SELECT e.ssn, pno
-    FROM works_on, employee as e
-    WHERE e.ssn = essn AND e.ssn != '123456789'));
-    
